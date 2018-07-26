f3fe763bc76f7ac211b166d6136420efd3f5569d
k-cormack <k.cormack@gmail.com>
2018-07-26 15:14:40 -0600

    Orb toggle fix

--- assets/app.js
+++ assets/app.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
             $('body').removeClass('white').addClass('black');
         }
     });
+    $('#orb').click(function(){
+        if ($('#orb').hasClass('sun')) {
+            $('#orb').removeClass('sun').addClass('moon');
+        }
+        else {
+            $('#orb').removeClass('moon').addClass('sun');
+        }
+    })
 });
